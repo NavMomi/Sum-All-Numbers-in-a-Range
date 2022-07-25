a6690c890aa056c8ac48dfa7515f1f03097c4aa4
Navjot Kaur <99341365+NavMomi@users.noreply.github.com>
2022-07-25 10:31:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,11 @@
 //We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
 
 function sumAll(arr) {
-    //console.log("this is the original rang " + arr);
-    //this for sort the rang.
     if (arr[0] > arr[1]) {
         let temp = arr[0];
         arr[0] = arr[1];
         arr[1] = temp;
-        // this for print the rang after sorted.
-        // console.log("this is the rang after sorted " + arr);
-    }
+  }
     // result here for sum all the rang and store it.
     let result = 0;
     for (let i = arr[0]; i <= arr[1]; i++) {
